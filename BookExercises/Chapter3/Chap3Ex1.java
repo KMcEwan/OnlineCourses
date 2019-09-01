606c40567a0855cbb631ec92cef4bfef92a7496b
KMcEwan <54743112+KMcEwan@users.noreply.github.com>
2019-09-01 15:54:35 +0100

    Add files via upload

--- BookExercises/Chapter3/Chap3Ex1.java
+++ BookExercises/Chapter3/Chap3Ex1.java
@@ -1,10 +1,14 @@
+/* 
+ * Program takes in two values, feet and inches, and outputs a value in CM.
+ */
+
 import acm.program.*;
 
 public class Chap3Ex1 extends ConsoleProgram
 {
 	public void run()
 	{
-		println("This program convers inches to centimeters.");
+		println("This program converts feet and inches to centimeters.");
 		int feet = readInt("Enter value in feet : ");
 		double inches = readDouble("Enter value in inches : ");
 		double ftToCm = feet * CM_PER_FOOT;
